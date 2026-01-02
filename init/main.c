955bb9939f638a5353276715c7aba3b4b7365f5b
GoRhanHee <gorhanhee01@gmail.com>
2026-01-02 16:33:15 +0900

    Revert "UPSTREAM: mm: init: report memory auto-initialization features at boot time"
    This reverts commit fce126b5740b32000e8b1e1fe5824a539b6ab082.

--- init/main.c
+++ init/main.c
@@ -609,7 +609,6 @@ static void __init mm_init(void)
 	 * bigger than MAX_ORDER unless SPARSEMEM.
 	 */
 	page_ext_init_flatmem();
-	report_meminit();
 	mem_init();
 	set_memsize_kernel_type(MEMSIZE_KERNEL_STOP);
 	kmem_cache_init();
