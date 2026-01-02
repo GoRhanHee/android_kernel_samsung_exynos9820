36f00e1e83ee153ba5fa3fc6dbe149c768f4b143
GoRhanHee <gorhanhee01@gmail.com>
2026-01-02 18:00:22 +0900

    build: add toolchain decompress code

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ TOOLCHAIN_FILE=$(basename "$TOOLCHAIN_URL")
 if [ ! -f "$TOOLCHAIN_FILE" ]; then
     wget -q --show-progress -O "$TOOLCHAIN_FILE" "$TOOLCHAIN_URL"
 fi
+tar -xf "$TOOLCHAIN_FILE" && rm "$TOOLCHAIN_FILE"
 
 # Cooking Kernel Source
 MAKE_ARGS="
