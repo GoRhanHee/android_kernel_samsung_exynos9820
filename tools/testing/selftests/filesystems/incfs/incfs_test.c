863d0643bd5c939bb2854ea035b96e73ceb6f8cc
GoRhanHee <gorhanhee01@gmail.com>
2026-01-21 17:55:08 +0900

    Revert "ANDROID: Make incfs selftests pass"
    This reverts commit c7ad1e2b5716c703d3f72308f938426834ccfc61.

--- tools/testing/selftests/filesystems/incfs/incfs_test.c
+++ tools/testing/selftests/filesystems/incfs/incfs_test.c
@@ -2398,9 +2398,7 @@ int main(int argc, char *argv[])
 	};
 #undef MAKE_TEST
 
-	/* Bring back for kernel 5.x */
-	/* ksft_set_plan(ARRAY_SIZE(cases)); */
-
+	ksft_set_plan(ARRAY_SIZE(cases));
 	for (i = 0; i < ARRAY_SIZE(cases); ++i) {
 		ksft_print_msg("Running %s\n", cases[i].name);
 		if (cases[i].pfunc(mount_dir) == TEST_SUCCESS)
