853030c219449af644af337165b77444999cbabf
GoRhanHee <gorhanhee01@gmail.com>
2026-01-21 15:30:55 +0900

    build: edit toolchain compiler

--- build.sh
+++ build.sh
@@ -96,32 +96,18 @@ export ARCH=arm64
 export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
 
-# Define toolchain variables
-CLANG_DIR=$PWD/toolchain/neutron_18
-PATH=$CLANG_DIR/bin:$PATH
+# Import Cross Compiler
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9  \
+ toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 
-# Symlinking python2
-if [ ! -f "$HOME/python" ]; then
-    ln -s /usr/bin/python2.7 "$HOME/python"
-fi 
+# Import clang-r383902
+git clone https://github.com/UniversalX-devs/prebuilts_clang_host_linux-x86_clang-r383902.git \
+ toolchain/clang/host/linux-x86/clang-r383902
 
-# Check if toolchain exists
-if [ ! -f "$CLANG_DIR/bin/clang-18" ]; then
-    echo "-----------------------------------------------"
-    echo "Toolchain not found! Downloading..."
-    echo "-----------------------------------------------"
-    rm -rf $CLANG_DIR
-    mkdir -p $CLANG_DIR
-    pushd toolchain/neutron_18 > /dev/null
-    bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S=05012024
-    echo "-----------------------------------------------"
-    echo "Patching toolchain..."
-    echo "-----------------------------------------------"
-    bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
-    echo "-----------------------------------------------"
-    echo "Cleaning up..."
-    popd > /dev/null
-fi
+# Setting toolchain path
+CLANG_DIR=${ANDROID_BUILD_TOP}/toolchain/clang/host/linux-x86/clang-r383902
+GCC_DIR=${ANDROID_BUILD_TOP}/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+PATH=$CLANG_DIR/bin:$CLANG_DIR/lib:$GCC_DIR/bin:$GCC_DIR/lib:$PATH
 
 # Cooking Kernel Source
 MAKE_ARGS="
@@ -129,11 +115,8 @@ LLVM=1 \
 LLVM_IAS=1 \
 ARCH=arm64 \
 -j16 \
-CC=clang \
-LD=ld.lld \
-CROSS_COMPILE=aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-CLANG_TRIPLE=aarch64-linux-gnu- \
+READELF=${CLANG_DIR}/bin/llvm-readelf \
+CROSS_COMPILE=${GCC_DIR}/bin/aarch64-linux-gnu- \
 O=out
 "
 
