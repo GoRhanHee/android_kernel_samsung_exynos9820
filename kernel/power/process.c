032e15a03c87a435899332bd24a33f1eb91f49b3
GoRhanHee <gorhanhee01@gmail.com>
2026-01-21 17:54:58 +0900

    Revert "kernel: power: omit trace markers and logspam"
    This reverts commit 5729f7303c6540941cbee056de81639e3d08b314.

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -26,9 +26,6 @@
 #include <linux/sec_debug.h>
 #include <linux/debug-snapshot.h>
 
-#undef trace_suspend_resume
-#define trace_suspend_resume(x, ...)
-
 /*
  * Timeout for stopping processes
  */
@@ -144,6 +141,9 @@ static int try_to_freeze_tasks(bool user_only)
 #if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
 		panic("fail to freeze tasks");
 #endif
+	} else {
+		pr_cont("(elapsed %d.%03d seconds) ", elapsed_msecs / 1000,
+			elapsed_msecs % 1000);
 	}
 
 	sec_debug_set_unfrozen_task((uint64_t)NULL);
@@ -174,11 +174,14 @@ int freeze_processes(void)
 		atomic_inc(&system_freezing_cnt);
 
 	pm_wakeup_clear(true);
+	pr_info("Freezing user space processes ... ");
 	pm_freezing = true;
 	error = try_to_freeze_tasks(true);
 	if (!error) {
 		__usermodehelper_set_disable_depth(UMH_DISABLED);
+		pr_cont("done.");
 	}
+	pr_cont("\n");
 	BUG_ON(in_atomic());
 
 	/*
@@ -207,10 +210,14 @@ int freeze_kernel_threads(void)
 {
 	int error;
 
+	pr_info("Freezing remaining freezable tasks ... ");
 
 	pm_nosig_freezing = true;
 	error = try_to_freeze_tasks(false);
+	if (!error)
+		pr_cont("done.");
 
+	pr_cont("\n");
 	BUG_ON(in_atomic());
 
 	if (error)
@@ -232,6 +239,7 @@ void thaw_processes(void)
 
 	oom_killer_enable();
 
+	pr_info("Restarting tasks ... ");
 
 	__usermodehelper_set_disable_depth(UMH_FREEZING);
 	thaw_workqueues();
@@ -252,6 +260,7 @@ void thaw_processes(void)
 	usermodehelper_enable();
 
 	schedule();
+	pr_cont("done.\n");
 	dbg_snapshot_suspend("thaw_processes", thaw_processes, NULL, 0, DSS_FLAG_OUT);
 	trace_suspend_resume(TPS("thaw_processes"), 0, false);
 }
@@ -261,6 +270,7 @@ void thaw_kernel_threads(void)
 	struct task_struct *g, *p;
 
 	pm_nosig_freezing = false;
+	pr_info("Restarting kernel threads ... ");
 
 	thaw_workqueues();
 
@@ -272,4 +282,5 @@ void thaw_kernel_threads(void)
 	read_unlock(&tasklist_lock);
 
 	schedule();
+	pr_cont("done.\n");
 }
