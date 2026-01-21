014863676fdc704ee8ae8cde0053cf21cf7914cf
GoRhanHee <gorhanhee01@gmail.com>
2026-01-21 17:55:06 +0900

    Revert "ANDROID: Incremental fs: Fix initialization, use of bitfields"
    This reverts commit 98bd3efad1cb995e4c8a00704d763a3b00f9550e.

--- include/uapi/linux/incrementalfs.h
+++ include/uapi/linux/incrementalfs.h
@@ -107,13 +107,10 @@ struct incfs_new_data_block {
 	/* Values from enum incfs_block_flags */
 	__u8 flags;
 
-	/* Reserved - must be 0 */
 	__u16 reserved1;
 
-	/* Reserved - must be 0 */
 	__u32 reserved2;
 
-	/* Reserved - must be 0 */
 	__aligned_u64 reserved3;
 };
 
@@ -143,7 +140,6 @@ struct incfs_file_signature_info {
 	/* Size of additional data. */
 	__u32 additional_data_size;
 
-	/* Reserved - must be 0 */
 	__u32 reserved1;
 
 	/*
@@ -157,7 +153,6 @@ struct incfs_file_signature_info {
 	/* Size of pkcs7 signature DER blob */
 	__u32 signature_size;
 
-	/* Reserved - must be 0 */
 	__u32 reserved2;
 
 	/* Value from incfs_hash_tree_algorithm */
@@ -181,10 +176,8 @@ struct incfs_new_file_args {
 	 */
 	__u16 mode;
 
-	/* Reserved - must be 0 */
 	__u16 reserved1;
 
-	/* Reserved - must be 0 */
 	__u32 reserved2;
 
 	/*
@@ -217,16 +210,13 @@ struct incfs_new_file_args {
 	 */
 	__u32 file_attr_len;
 
-	/* Reserved - must be 0 */
 	__u32 reserved4;
 
 	/* struct incfs_file_signature_info *signature_info; */
 	__aligned_u64 signature_info;
 
-	/* Reserved - must be 0 */
 	__aligned_u64 reserved5;
 
-	/* Reserved - must be 0 */
 	__aligned_u64 reserved6;
 };
 
