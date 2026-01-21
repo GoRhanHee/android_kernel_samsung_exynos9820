c150611b4c5183dc399427a3aaf37dd0b2c2d582
GoRhanHee <gorhanhee01@gmail.com>
2026-01-21 17:55:04 +0900

    Reapply "ANDROID: Incremental fs: Fix initialization, use of bitfields"
    This reverts commit 44d3bf62401ba083562b58061cd3c3a4fee75090.

--- include/uapi/linux/incrementalfs.h
+++ include/uapi/linux/incrementalfs.h
@@ -150,10 +150,13 @@ struct incfs_fill_block {
 	/* Values from enum incfs_block_flags */
 	__u8 flags;
 
+	/* Reserved - must be 0 */
 	__u16 reserved1;
 
+	/* Reserved - must be 0 */
 	__u32 reserved2;
 
+	/* Reserved - must be 0 */
 	__aligned_u64 reserved3;
 };
 
@@ -203,8 +206,10 @@ struct incfs_new_file_args {
 	 */
 	__u16 mode;
 
+	/* Reserved - must be 0 */
 	__u16 reserved1;
 
+	/* Reserved - must be 0 */
 	__u32 reserved2;
 
 	/*
@@ -237,6 +242,7 @@ struct incfs_new_file_args {
 	 */
 	__u32 file_attr_len;
 
+	/* Reserved - must be 0 */
 	__u32 reserved4;
 
 	/*
@@ -264,6 +270,7 @@ struct incfs_new_file_args {
 	/* Size of signature_info */
 	__aligned_u64 signature_size;
 
+	/* Reserved - must be 0 */
 	__aligned_u64 reserved6;
 };
 
