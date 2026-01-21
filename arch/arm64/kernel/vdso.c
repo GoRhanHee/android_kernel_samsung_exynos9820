fce2a5bbb5f39b148bc8bd4e672c221dca5578f3
GoRhanHee <gorhanhee01@gmail.com>
2026-01-21 17:55:39 +0900

    Revert "arm64: Fix vdso.c"
    This reverts commit 284e4fc0099ab8c822cfa53ecdce40f04bc69d71.

--- arch/arm64/kernel/vdso.c
+++ arch/arm64/kernel/vdso.c
@@ -172,7 +172,7 @@ static int __init vdso_mappings_init(const char *name,
 	struct page **vdso_pagelist;
 	unsigned long pfn;
 
-	if (memcmp(code_start, "\177ELF", 4)) {
+	if (memcmp(vdso_start, "\177ELF", 1)) {
 		pr_err("vDSO is not a valid ELF object!\n");
 		return -EINVAL;
 	}
