f947838b42f689b180868b78b3e9391f90a9ca5b
GoRhanHee <gorhanhee01@gmail.com>
2026-01-21 17:54:34 +0900

    Revert "workflow: edit install python2 scripts"
    This reverts commit 46baa85b0e0e6ef8fec143bd2c10feea3fcea88e.

--- python2.sh
+++ python2.sh
@@ -1,19 +1,7 @@
-# Setting Path
-export ANDROID_BUILD_TOP=$(pwd)
-
 # Install Python2
-cd /usr/src
 wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
 tar xzf Python-2.7.18.tgz
 cd Python-2.7.18
-./configure --prefix=/usr/local/python2.7
-make -j16
-sudo make install
-/usr/local/python2.7/bin/python2.7 -V
-echo 'export PATH="/usr/local/python2.7/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-python2.7 -V
-
-sudo ln -sf /usr/local/python2.7/bin/python2.7 /usr/local/bin/python
-
-cd ${ANDROID_BUILD_TOP}
+sudo ./configure --enable-optimizations
+sudo make altinstall
+sudo ln -s "/usr/local/bin/python2.7" "/usr/bin/python"
