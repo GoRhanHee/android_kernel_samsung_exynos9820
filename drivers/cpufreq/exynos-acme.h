62d10b2b481707c8773a5d0ea7f61a765ec0fd77
GoRhanHee <gorhanhee01@gmail.com>
2026-01-21 17:55:33 +0900

    Revert "Exynos 9820 : Configurable OC and UC"
    This reverts commit 478e9cce36ab9fca0cdb66f1777e943e7e21ca1b.

--- drivers/cpufreq/exynos-acme.h
+++ drivers/cpufreq/exynos-acme.h
@@ -58,10 +58,6 @@ struct exynos_cpufreq_domain {
 
 	unsigned int			max_freq;
 	unsigned int			min_freq;
-#ifdef CONFIG_ARM_MODCLOCK
-	unsigned int			max_usable_freq;
-	unsigned int			min_usable_freq;
-#endif	
 	unsigned int			boot_freq;
 	unsigned int			resume_freq;
 	unsigned int			old;
@@ -91,7 +87,7 @@ struct exynos_cpufreq_domain {
 	/* list head of DVFS Manager constraints */
 	struct list_head		dm_list;
 
-	/* list head of User cpuFreq Ctrl (UFC - User Frequency Control) */
+	/* list head of User cpuFreq Ctrl (UFC) */
 	struct list_head		ufc_list;
 
 	bool				need_awake;
